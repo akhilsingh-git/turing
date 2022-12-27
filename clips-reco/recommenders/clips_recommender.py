e3a973ae3707d38a8c92bd11a70b9cf6a252ad16
Rajdeep Singh <rajdeep.singh@pocketaces.in>
2022-12-27 17:10:58 +0530

    Change in serving quantity Changelog: 1. Only top 20 videos from watch history are considered 2. Result set includes top 1000 clips only

--- clips-reco/recommenders/clips_recommender.py
+++ clips-reco/recommenders/clips_recommender.py
@@ -12,6 +12,10 @@ G = None
 ScoreMatrix = None
 
 
+def get_clip_max_score(clip_uid):
+    return ScoreMatrix[clip_uid]["Maxscore"]
+
+
 def init_clips_models():
     global G
     global ScoreMatrix
@@ -33,29 +37,41 @@ def get_all_recommended_clips(user_id):
     params:
         user_id | Format: "u_{user_id}"
     """
-    result_clip_to_score_maping = {}
+    result_clip_to_score_mapping = {}
 
     # Already watched videos by the user
     # Embedded in the model while training
     lifetime_watch_history = list(set(G.neighbors(user_id)))
+    lifetime_watch_history.sort(reverse=True, key=get_clip_max_score)
 
-    # Todo: Might need to shift to top 100 only
-    for source_clip_id in lifetime_watch_history:
+    for source_clip_id in lifetime_watch_history[:20]:
         related_score_matrix = ScoreMatrix[source_clip_id]
         related_clips_ids = list(related_score_matrix.keys())
 
         for suggested_clip_id in related_clips_ids:
-            if suggested_clip_id not in result_clip_to_score_maping:
-                result_clip_to_score_maping[suggested_clip_id] = related_score_matrix[suggested_clip_id]
+            if suggested_clip_id not in result_clip_to_score_mapping:
+                result_clip_to_score_mapping[suggested_clip_id] = related_score_matrix[suggested_clip_id]
 
-            result_clip_to_score_maping[suggested_clip_id] = max(
-                result_clip_to_score_maping[suggested_clip_id], related_score_matrix[suggested_clip_id]
+            result_clip_to_score_mapping[suggested_clip_id] = max(
+                result_clip_to_score_mapping[suggested_clip_id], related_score_matrix[suggested_clip_id]
             )
 
     for watched_clip in lifetime_watch_history:
-        result_clip_to_score_maping[watched_clip] = -1
+        result_clip_to_score_mapping[watched_clip] = -1
 
-    if "Maxscore" in result_clip_to_score_maping:
-        del result_clip_to_score_maping["Maxscore"]
+    if "Maxscore" in result_clip_to_score_mapping:
+        del result_clip_to_score_mapping["Maxscore"]
 
-    return result_clip_to_score_maping
+    results = list(result_clip_to_score_mapping.keys())
+
+    def get_result_mapping_scores(clip_uid):
+        return result_clip_to_score_mapping[clip_uid]
+
+    results.sort(reverse=True, key=get_result_mapping_scores)
+    results = results[:1000]
+
+    final_clips_scores = {}
+    for clip_uid in results:
+        final_clips_scores[clip_uid] = result_clip_to_score_mapping[clip_uid]
+
+    return final_clips_scores
