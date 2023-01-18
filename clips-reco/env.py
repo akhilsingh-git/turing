83a5947a780182e41d795e39ba5ea2c867219731
Rajdeep Singh <rajdeep.singh@pocketaces.in>
2023-01-18 19:00:27 +0530

    Updates for models metadata in dockerfile

--- clips-reco/env.py
+++ clips-reco/env.py
@@ -1,36 +1,23 @@
 import os
+import json
 
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 LOG_FILE_PATH = os.environ.get("LOG_FILE_PATH", "app.log")
 
-VIDEO_SCORE_MATRIX_MODEL_DATE = os.environ["video_score_matrix_model_date"]
-VIDEO_SCORE_MATRIX_MODEL_TYPE = os.environ["video_score_matrix_model_type"]
-VIDEO_SCORE_MATRIX_MODEL_ARCH = os.environ["video_score_matrix_model_arch"]
-VIDEO_SCORE_MATRIX_MODEL_ARCHIVE_NAME = os.environ["video_score_matrix_model_archive_name"]
-VIDEO_SCORE_MATRIX_MODEL_CONTENT = os.environ["video_score_matrix_model_content"]
 
-WATCH_HISTORY_GRAPH_MODEL_DATE = os.environ["watch_history_graph_model_date"]
-WATCH_HISTORY_GRAPH_MODEL_TYPE = os.environ["watch_history_graph_model_type"]
-WATCH_HISTORY_GRAPH_MODEL_ARCH = os.environ["watch_history_graph_model_arch"]
-WATCH_HISTORY_GRAPH_MODEL_ARCHIVE_NAME = os.environ["watch_history_graph_model_archive_name"]
-WATCH_HISTORY_GRAPH_MODEL_CONTENT = os.environ["watch_history_graph_model_content"]
+def get_model_metadata(model_name):
+    file_path = "./models_meta/{}.json".format(model_name)
 
-MODELS_METADATA_RESPONSE = {
-    "video_score_matrix": {
-        "name": "video_score_matrix",
-        "date": VIDEO_SCORE_MATRIX_MODEL_DATE,
-        "type": VIDEO_SCORE_MATRIX_MODEL_TYPE,
-        "arch": VIDEO_SCORE_MATRIX_MODEL_ARCH,
-        "archive_name": VIDEO_SCORE_MATRIX_MODEL_ARCHIVE_NAME,
-        "content": VIDEO_SCORE_MATRIX_MODEL_CONTENT,
-    },
-    "watch_history_graph": {
-        "name": "watch_history_graph",
-        "date": WATCH_HISTORY_GRAPH_MODEL_DATE,
-        "type": WATCH_HISTORY_GRAPH_MODEL_TYPE,
-        "arch": WATCH_HISTORY_GRAPH_MODEL_ARCH,
-        "archive_name": WATCH_HISTORY_GRAPH_MODEL_ARCHIVE_NAME,
-        "content": WATCH_HISTORY_GRAPH_MODEL_CONTENT,
-    },
-}
+    with open(file_path, "r") as f:
+        parsed_data = json.load(f)
+        result = {
+            "name": model_name,
+            "data_date": parsed_data["Metadata"]["model_data_date"],
+            "type": parsed_data["Metadata"]["model_type"],
+            "arch": parsed_data["Metadata"]["model_arch"],
+            "archive_name": parsed_data["Metadata"]["model_archive_name"],
+            "content": parsed_data["Metadata"]["model_content"],
+        }
+
+    return result
